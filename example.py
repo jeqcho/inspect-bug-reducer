f3da8cc1f4cb7feff331b1384476d76c101cb8c2
jeqcho <jeqin_chooi@college.harvard.edu>
2025-09-14 14:33:01 -0400

    grouped

--- example.py
+++ example.py
@@ -1,26 +1,28 @@
+import random
 from inspect_ai import Task, task
-from inspect_ai._eval.task.epochs import Epochs
-from inspect_ai.dataset import example_dataset
+from inspect_ai.dataset import Dataset, example_dataset
 
-from inspect_ai.scorer import match, score_reducer
-from inspect_ai.scorer._metric import Score
-from inspect_ai.scorer._reducer.types import ScoreReducer
+from inspect_ai.scorer import accuracy, grouped, match, stderr
 from inspect_ai.solver import generate
 
 
 @task
-def task_using_custom_reducer():
+def some_task():
     return Task(
-        dataset=example_dataset("theory_of_mind"),
+        dataset=load_dataset(),
         solver=[generate()],
         scorer=match(),
-        epochs=Epochs(10, ["a_custom_score_reducer"]),
+        metrics=[
+            grouped(accuracy(), group_key="category", all_label="all (accuracy)"),
+            grouped(stderr(), group_key="category", all_label="all (stderr)"),
+        ],
     )
 
 
-@score_reducer
-def a_custom_score_reducer() -> ScoreReducer:
-    def reduce(scores: list[Score]) -> Score:
-        return Score(value=0)
-
-    return reduce
+def load_dataset() -> Dataset:
+    dataset = example_dataset("theory_of_mind")
+    for sample in dataset:
+        if sample.metadata is None:
+            sample.metadata = {}
+        sample.metadata["category"] = random.choice(["red", "green"])
+    return dataset
